651d32035e4eeef2afc4fc58dc28dc2654a0b1a1
J-sankar <jayasankarmenonv@gmail.com>
2025-09-01 20:55:21 +0530

    Add cookie-parser for setting and using cookies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,7 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import authRouter from './routes/authRoutes.js'
 import errorHandler from './middlewares/errorHandler.js'
+import cookieParser from 'cookie-parser'
 const app = express()
+app.use(cookieParser())
 
 app.use(express.json())
 app.use(cors({

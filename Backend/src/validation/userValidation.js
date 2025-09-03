29ac0f07ea8bdadc75139b5d4399137da98e4e99
J-sankar <jayasankarmenonv@gmail.com>
2025-09-03 15:37:35 +0530

    alter joi schema to accept null and empty last names

--- Backend/src/validation/userValidation.js
+++ Backend/src/validation/userValidation.js
@@ -10,8 +10,8 @@ export const oauthSchema = Joi.object({
 
 export const signupSchema = Joi.object({
     email:  Joi.string().email({minDomainSegments:2, tlds:{allow:['com','net']}}).required(),
-    firstname: Joi.string().required(),
-    lastname: Joi.string().optional(),
+    firstname: Joi.string().pattern(/^[A-Za-z\s]+$/).required(),
+    lastname: Joi.string().pattern(/^[A-Za-z\s]+$/).optional().allow(''),
     phoneno: Joi.string().pattern(/^\+?[0-9]{10,15}$/).optional(),
     password: Joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/).required()
 

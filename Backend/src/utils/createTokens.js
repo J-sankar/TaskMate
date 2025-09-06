54844a19b949f141087e33a8e756fe3a610002a5
J-sankar <jayasankarmenonv@gmail.com>
2025-09-06 23:51:52 +0530

    add: time of token creation return during refresh token creation

--- Backend/src/utils/createTokens.js
+++ Backend/src/utils/createTokens.js
@@ -5,23 +5,34 @@ import { env } from '../config/env.js'
 
 
 export const createRefreshToken = (user,deviceId) => {
-    const refreshToken = jwt.sign(
-        {
-            id: user.userId,
-            deviceId
-
-        }, env.REFRESH_SECRET,
-        { expiresIn: '14d' }
-
-    )
-    return refreshToken
+    try {
+        
+        const refreshToken = jwt.sign(
+            {
+                id: user.userId,
+                deviceId
+    
+            }, env.REFRESH_SECRET,
+            { expiresIn: '14d' }
+    
+        )
+        const tokenCreatedAt = new Date()
+        return {refreshToken,tokenCreatedAt}
+    } catch (error) {
+        throw new Error("Failed to create refresh token")
+    }
 }
 
 export const createAccessToken = (user) =>{
-    const accessToken = jwt.sign(
-        {id:user.userId,email:user.email,role:user.role,status:user.status},
-        env.ACCESS_SECRET,
-        {expiresIn:'15min'}
-    )
-    return accessToken
+    try {
+        
+        const accessToken = jwt.sign(
+            {id:user.userId,role:user.role},
+            env.ACCESS_SECRET,
+            {expiresIn:'15min'}
+        )
+        return accessToken
+    } catch (error) {
+        throw new Error("failed to create access token")
+    }
 }

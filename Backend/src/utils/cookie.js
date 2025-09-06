413ca0a612e736dbce4745d35981d3a404f21ebb
J-sankar <jayasankarmenonv@gmail.com>
2025-09-06 23:50:31 +0530

    add:utility to set access token in cookie

--- Backend/src/utils/cookie.js
+++ Backend/src/utils/cookie.js
@@ -1,6 +1,6 @@
 import { env } from "../config/env.js";
 
-const setRefreshTokenInCookie = (res, refreshToken) => {
+export const setRefreshTokenInCookie = (res, refreshToken) => {
     res.cookie('refreshToken', refreshToken,
         {
             httpOnly: true,
@@ -10,4 +10,15 @@ const setRefreshTokenInCookie = (res, refreshToken) => {
         }
     )
 }
-export default setRefreshTokenInCookie
+export const setAccessTokenInCookie = (res,accessToken)=>{
+    res.cookie('accessToken', accessToken,
+        {
+            httpOnly:true,
+            secure:env.NODE_ENV === 'production',
+            sameSite: 'strict',
+             maxAge:  15 * 60 * 1000
+        }
+    )
+}
+
+

93ddf32eec7ef47771c52d01dc6d0b8fab799993
J-sankar <jayasankarmenonv@gmail.com>
2025-09-07 23:35:34 +0530

    add:new style components to Dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { useEffect } from 'react'
-import DashboardNavbar from '../components/dashboardNavbar'
-import Sidebar from '../components/Sidebar'
+import DashboardNavbar from '../components/Dashboard/DashboardNavbar'
+import Sidebar from '../components/Dashboard/Sidebar'
+import MainSection from '../components/Dashboard/MainSection'
 
 const Dashboard = () => {
  
   
   return (
-    <div className='flex gap-1.5 justify-center p-2 bg-white'>
+    
+    <div className='flex gap-1.5 min-h-screen px-2.5 py-1.5 bg-white'>
       <Sidebar />
+      <div className='flex flex-col flex-1'>
+
       <DashboardNavbar />
+      <MainSection className/>
+      </div>
      
     </div>
   )

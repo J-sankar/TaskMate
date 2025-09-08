e4f117edee91947877644015c473eb576b996f39
J-sankar <jayasankarmenonv@gmail.com>
2025-09-09 00:55:07 +0530

    fix:issues in setting cookie

--- Backend/src/utils/cookie.js
+++ Backend/src/utils/cookie.js
@@ -6,7 +6,7 @@ export const setRefreshTokenInCookie = (res, refreshToken) => {
             httpOnly: true,
             secure: env.NODE_ENV === 'production',
             sameSite: 'strict',
-            maxAge: 14 * 24 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000
         }
     )
 }

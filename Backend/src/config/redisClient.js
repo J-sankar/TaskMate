3bac29a3ab80d4674c511e26381509045674eb38
J-sankar <jayasankarmenonv@gmail.com>
2025-09-09 00:52:16 +0530

    add: redisClient to configuration

---
+++ Backend/src/config/redisClient.js
@@ -0,0 +1,16 @@
+import {createClient} from 'redis'
+
+const redis = createClient({
+    socket:{
+     host: "127.0.0.1",
+    port:6379
+}
+})
+
+redis.on('error',(err)=>{
+    console.error(err) 
+})
+
+await redis.connect()
+
+export default redis

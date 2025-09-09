8a3500577ba8b060f20f396298a391acc2635323
J-sankar <jayasankarmenonv@gmail.com>
2025-09-10 00:47:45 +0530

    feat: add auth middleware for token validation

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { findUserById} from "../services/userServices.js"
+import { verifyAccessToken } from "../utils/verifiyTokens.js"
+
+const authMiddleware = async (req, res, next)=>{
+    const accessToken = req.cookies.accessToken
+    if (!accessToken){
+        const err = new Error("Access Token Missing")
+        err.status = 401
+        return next(err)
+    }
+    try {
+        const payload = verifyAccessToken(accessToken)
+        if (!payload){
+            const err = new Error("Invalid or Expired token")
+            err.status = 401
+            return next(err)
+        }
+        const user = await findUserById(payload.id)
+        if (!user) {
+            const err = new Error("User not found")
+            err.status = 404
+            return next(err)
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        return next(error)
+    }
+}
